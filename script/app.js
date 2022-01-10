042765df3b4c0f5d65a5363a1286efd38cce3f07
danil-vladimirov <46453744+danil-vladimirov@users.noreply.github.com>
2022-01-10 18:41:12 +0300

    bg color and styles

--- script/app.js
+++ script/app.js
@@ -1,6 +1,3 @@
 let body = document.body;
 
-let r = Math.floor(Math.random() * 255); 
-let g = Math.floor(Math.random() * 255);
-let b = Math.floor(Math.random() * 255);
-body.style.setProperty('background-color', 'rgb(' +r+ ',' +g+ ',' +b+ ')');
+body.style.setProperty('background-color', "hsl(" + 360 * Math.random() + ',' + (25 + 70 * Math.random()) + '%,' + (85 + 10 * Math.random()) + '%)');
